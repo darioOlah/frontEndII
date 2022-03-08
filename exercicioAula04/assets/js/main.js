e9fbfb5cc77c0dd7fb0644682a947a31dfcf1a5e
darioOlah <omdracing@gmail.com>
2022-03-07 22:13:15 -0300

    Enviando Ex: Aula 04 (07/03/2022)

---
+++ exercicioAula04/assets/js/main.js
@@ -0,0 +1,16 @@
+var todosOsItens = document.querySelectorAll(".lista__item");
+
+todosOsItens[0].classList.add('removido');
+todosOsItens[0].innerText = "Carro - Removido";
+
+todosOsItens[1].classList.add('selecionado');
+todosOsItens[1].innerText = "Bike - Selecionado";
+
+todosOsItens[2].classList.add('selecionado');
+todosOsItens[2].innerText = "Patinete - Selecionado";
+
+todosOsItens[3].classList.add('removido');
+todosOsItens[3].innerText = "Celular - Removido";
+
+todosOsItens[4].classList.add('removido');
+todosOsItens[4].innerText = "Carregador - Removido";
